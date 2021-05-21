5e4cc915aafd943d1b828ac688d4188f0240051f
Scott Worley <scott.worley@microchip.com>
2021-05-21 15:56:00 -0400

    soc: mchp: Add MEC172x folder and files
    Add folder and files for Microchip MEC172x series SoC.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

---
+++ soc/arm/microchip_mec/mec172x/soc.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2019 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __MEC_SOC_H
+#define __MEC_SOC_H
+
+#define SYSCLK_DEFAULT_IOSC_HZ MHZ(96)
+
+#ifndef _ASMLANGUAGE
+
+#include "MEC172XNSZ.h"
+#include "mec_defs.h"
+#include "regaccess.h"
+
+#include "../common/soc_gpio.h"
+#include "../common/soc_pins.h"
+#include "../common/soc_espi_channels.h"
+#include "../common/soc_espi_saf.h"
+
+uint32_t soc_get_core_clock(void);
+
+#endif
+
+#endif
