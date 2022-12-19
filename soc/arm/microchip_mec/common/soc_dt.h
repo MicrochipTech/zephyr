d4f878d05b27796a34b10643952735cc8437fdc4
scott worley <scott.worley@microchip.com>
2022-12-19 16:20:35 -0500

    drivers: DMA: Microchip XEC DMA driver
    DNM (DO NOT MERGE) WIP(Work in Progress)
    Microchip XEC devices include a multi-channel DMA
    device capable of memory to memory, peripheral to
    memory, and memory to peripheral transfers. The DMA
    controller is hardwired for a subset of peripherals:
    I2C and SPI controllers.
    
    Signed-off-by: scott worley <scott.worley@microchip.com>

--- soc/arm/microchip_mec/common/soc_dt.h
+++ soc/arm/microchip_mec/common/soc_dt.h
@@ -23,4 +23,21 @@
 #define MCHP_XEC_DRVSTR_12MA	0x4
 #define MCHP_XEC_FUNC_INVERT	0x1
 
+/* Obtain DMA channel DT information */
+#define MCHP_XEC_DT_INST_DMA_CELL(n, name, cell)			\
+	COND_CODE_1(DT_INST_NODE_HAS_PROP(n, dmas),			\
+		    (DT_INST_DMAS_CELL_BY_NAME(n, name, cell)), (0xff))
+
+#define MCHP_XEC_DT_INST_DMA_TRIGSRC(n, name)				\
+	MCHP_XEC_DT_INST_DMA_CELL(n, name, trigsrc)
+
+#define MCHP_XEC_DT_INST_DMA_CHANNEL(n, name)				\
+	MCHP_XEC_DT_INST_DMA_CELL(n, name, channel)
+
+/* Extracts node id of DMA controller with a name property */
+#define MCHP_XEC_DT_INST_DMA_CTLR(n, name)				\
+	COND_CODE_1(DT_INST_NODE_HAS_PROP(n, dmas),			\
+		    (DT_INST_DMAS_CTLR_BY_NAME(n, name)),		\
+		    (DT_INVALID_NODE))
+
 #endif /* _MICROCHIP_XEC_SOC_DT_H_ */
