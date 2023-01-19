18ab55ec9bb31c332d0745a215e0513c48993a36
scott worley <scott.worley@microchip.com>
2023-01-19 10:49:27 -0500

    samples: drivers: SPI: Microchip XEC GS-SPI sample updates
    Remove SPI extended mode configuration enable since GP-SPI
    hardware supports dual I/O for receive only, not transmit.
    The GP-SPI driver will only support full-duplex.
    Change test to erase, program, then read.
    
    Signed-off-by: scott worley <scott.worley@microchip.com>

--- samples/drivers/spi_microchip_xec_gpspi/src/main.c
+++ samples/drivers/spi_microchip_xec_gpspi/src/main.c
@@ -7,17 +7,15 @@
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
-#include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/spi.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log_ctrl.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(spi_xec_gpspi, CONFIG_SPI_LOG_LEVEL);
 
 /**
  * @file Sample app using the Microchip GPSPI driver with a W25Q128 SPI flash.
- */
-
-/* NOTE 1 Requires Zephyr SPI driver has support for CONFIG_SPI_EXTENDED_MODES
- * and SPI_LINES_DUAL/SPI_LINES_QUAD. Microchip XEC QMSPI controller supports
- * dual and quad. XEC GPSPI controller can do dual (no quad).
  */
 
 #define W25Q128_JEDEC_ID 0x001840efU
@@ -65,6 +63,8 @@
 
 #define SPI1_NODE	DT_NODELABEL(spi1)
 
+#define SPI_FLASH_TEST_ADDR 0x00320100u
+#define SPI_FLASH_TEST_DATA_LEN 16
 
 struct spi_flash_info {
 	uint32_t jedec_id;
@@ -97,6 +97,7 @@ struct spi_flash_info const *lookup_spi_flash(uint32_t jedec_id)
 	return NULL;
 }
 
+/* 24-bit flash address only */
 static int spi_flash_format_cmd_addr(uint8_t *buf, size_t bufsz, uint8_t cmd, uint32_t spi_addr)
 {
 	size_t bsz = bufsz;
@@ -287,80 +288,6 @@ static int spi_flash_read(const struct device *spi,
 	return err;
 }
 
-static int spi_flash_read_dual(const struct device *spi, struct spi_config *spi_cfg, uint8_t cmd,
-			       uint32_t spi_addr, uint8_t *data, size_t datasz)
-{
-	int err;
-	uint32_t temp;
-	uint8_t txbuf[4];
-	struct spi_config fd_cfg;
-	struct spi_config dual_cfg;
-	struct spi_buf tb[1];
-	struct spi_buf rb[2];
-
-	if (!spi || !spi_cfg || !data || !datasz) {
-		return -EINVAL;
-	}
-
-	memset(txbuf, 0, sizeof(txbuf));
-	memcpy(&fd_cfg, spi_cfg, sizeof(struct spi_config));
-	memcpy(&dual_cfg, spi_cfg, sizeof(struct spi_config));
-	fd_cfg.operation |= SPI_HOLD_ON_CS;
-	dual_cfg.operation = (dual_cfg.operation & ~SPI_LINES_MASK) | SPI_LINES_DUAL;
-
-	/* 1. Transmit opcode || 24-bit SPI address || 1-byte(tri-state clocks)
-	 *    using full-duplex mode.
-	 * 2. Change config to dual to read data
-	 */
-
-	/* assuming 24-bit SPI address */
-	txbuf[0] = cmd;
-	txbuf[1] = (uint8_t)(spi_addr >> 16);
-	txbuf[2] = (uint8_t)(spi_addr >> 8);
-	txbuf[3] = (uint8_t)(spi_addr >> 0);
-
-	/* Try. One TX buffer for command and address.
-	 * Two RX buffers. First is 1 byte which is the tri-state clocks.
-	 * Second is data.
-	 */
-	temp = 0;
-
-	tb[0].buf = txbuf;
-	tb[0].len = 4;
-
-	const struct spi_buf_set txs = {
-		.buffers = tb,
-		.count = 1,
-	};
-
-	err = spi_transceive(spi, &fd_cfg, &txs, NULL);
-	if (err) {
-		return err;
-	}
-
-	/* GPSPI driver ignores spi_buf's with NULL pointer or data length == 0.
-	 * Dual opcode 0x3b requires 8 SPI clocks with I/O tri-stated after
-	 * SPI address is clocked out. Using TX for the 8 clocks results in
-	 * GPSPI controller driving the MOSI pin and at some time during the
-	 * 8 clocks the SPI flash will start driving the MOSI pin. We do the 8
-	 * tri-state clocks using RX. In dual mode GPSPI issues 4 clocks per
-	 * byte. Our first spi_buf uses a temp buffer and length = 2 bytes.
-	 */
-	rb[0].buf = &temp;
-	rb[0].len = 2;
-	rb[1].buf = data;
-	rb[1].len = datasz;
-
-	const struct spi_buf_set rxs_dual = {
-		.buffers = rb,
-		.count = 2,
-	};
-
-	err = spi_transceive(spi, &dual_cfg, NULL, &rxs_dual);
-
-	return err;
-}
-
 static int spi_flash_erase(const struct device *spi, struct spi_config *spi_cfg,
 			   uint8_t cmd, uint32_t spi_addr)
 {
@@ -377,12 +304,12 @@ static int spi_flash_erase(const struct device *spi, struct spi_config *spi_cfg,
 
 	switch (cmd) {
 	case SPI_FLASH_ERASE_SECTOR_CMD:
-		spi_addr &= ~0xffu; /* align on page boundary */
+		spi_addr &= ~0xfffu; /* align on 4KB */
 		break;
-	case SPI_FLASH_ERASE_BLOCK1_CMD: /* 32kb */
+	case SPI_FLASH_ERASE_BLOCK1_CMD: /* 32KB */
 		spi_addr &= ~0x7fffu;
 		break;
-	case SPI_FLASH_ERASE_BLOCK2_CMD: /* 64kb */
+	case SPI_FLASH_ERASE_BLOCK2_CMD: /* 64KB */
 		spi_addr &= ~0xffffu;
 		break;
 	case SPI_FLASH_ERASE_CHIP_CMD:
@@ -537,24 +464,6 @@ static int buf_contains(uint8_t *data, size_t datasz, uint8_t val)
 	return 1;
 }
 
-static void pr_data_buf(uint8_t *data, size_t datasz)
-{
-	printf("Data @ 0x%08x len = %u\n", (uint32_t)data, datasz);
-
-	if (!data || !datasz) {
-		return;
-	}
-
-	int count = 0;
-	for (size_t n = 0; n < datasz; n++) {
-		printf("0x%02x ", data[n]);
-		if (++count == 8) {
-			count = 0;
-			printf("\n");
-		}
-	}
-}
-
 #ifdef CONFIG_SPI_ASYNC
 
 struct spi_user_data {
@@ -579,15 +488,8 @@ static int spi_flash_read_async(const struct device *spi,
 				uint32_t spi_addr, uint8_t *data, size_t datasz)
 {
 	int err;
-#if 0 /* working */
-	uint8_t txdata1[4] = {0};
-	uint8_t rxdata1[4] = {0};
-	struct spi_buf txb[2] = {0};
-	struct spi_buf rxb[2] = {0};
-#else /* should work but failed due to not waiting for done before returning */
 	uint8_t txdata1[4] = {0};
 	struct spi_buf sb[2] = {0};
-#endif
 	struct spi_user_data ud = {0};
 	uint64_t wait_count = 0;
 
@@ -600,27 +502,7 @@ static int spi_flash_read_async(const struct device *spi,
 	if (err) {
 		return err;
 	}
-#if 0
-	txb[0].buf = &txdata1;
-	txb[0].len = 4;
-	txb[1].buf = data;
-	txb[1].len = datasz;
 
-	rxb[0].buf = &rxdata1;
-	rxb[0].len = 4;
-	rxb[1].buf = data;
-	rxb[1].len = datasz;
-
-	const struct spi_buf_set txs = {
-		.buffers = txb,
-		.count = 2,
-	};
-
-	const struct spi_buf_set rxs = {
-		.buffers = rxb,
-		.count = 2,
-	};
-#else
 	sb[0].buf = &txdata1;
 	sb[0].len = 4;
 	sb[1].buf = data;
@@ -635,7 +517,6 @@ static int spi_flash_read_async(const struct device *spi,
 		.buffers = sb,
 		.count = 2,
 	};
-#endif
 
 	err = spi_transceive_cb(spi, spi_cfg, &txs, &rxs, spi_callback, (void *)&ud);
 	if (err == 0) {
@@ -643,8 +524,8 @@ static int spi_flash_read_async(const struct device *spi,
 		while (!spi_cb_called) {
 			wait_count++;
 		}
-		printf("Asynchronous read completed after %llu spin loops\n", wait_count);
-		printf("Callback done = %u  result = %u\n", ud.done, ud.result);
+		LOG_INF("Asynchronous read completed after %llu spin loops", wait_count);
+		LOG_INF("Callback done = %u  result = %u", ud.done, ud.result);
 	}
 
 	return err;
@@ -665,14 +546,14 @@ void main(void)
 	uint8_t cmd;
 	uint8_t spi_status;
 
-	printf("Microchip XEC GPSPI example application\n");
+	LOG_INF("Microchip XEC GPSPI example application");
 #ifdef CONFIG_DMA
-	printf("GPSPI driver DMA support enabed in build\n");
+	LOG_INF("GPSPI driver DMA support enabed in build");
 #endif
 
 	spi = DEVICE_DT_GET(DT_ALIAS(gpspi));
 	if (!device_is_ready(spi)) {
-		printk("SPI device %s is not ready\n", spi->name);
+		LOG_ERR("SPI device %s is not ready", spi->name);
 		return;
 	}
 
@@ -686,250 +567,196 @@ void main(void)
 	spi_cfg.cs = &cs_ctrl;
 #endif
 	spi_cfg.operation = SPI_WORD_SET(8);
-	spi_cfg.frequency = MHZ(4);
+	spi_cfg.frequency = MHZ(12);
 
-	printf("Read SPI flash JEDEC ID\n");
+	LOG_INF("Read SPI flash JEDEC ID");
 
 	jedec_id = 0x55555555U;
 	err = spi_flash_read_id(spi, &spi_cfg, &jedec_id);
 	if (err) {
-		printk("Could not read SPI flash ID. error = %d\n", err);
+		LOG_ERR("Could not read SPI flash ID. error = %d", err);
 		return;
 	}
 
-	printf("Read SPI device JEDEC_ID as 0x%08x\n", jedec_id);
+	jedec_id &= SPI_FLASH_JEDEC_ID_MSK;
+	LOG_INF("Read SPI device JEDEC_ID as 0x%08x", jedec_id);
 
 	flash = lookup_spi_flash(jedec_id);
 	if (!flash) {
-		printk("Unknown SPI flash device. Test aborting.\n");
+		LOG_ERR("Unknown SPI flash device. Test aborting.");
 		return;
 	}
 
-	printf("SPI Flash is %s\n", flash->name);
+	LOG_INF("SPI Flash is %s", flash->name);
 
-	printf("Read SPI flash status1\n");
+	LOG_INF("Read SPI flash status1");
 	spi_status = 0x55u;
 	cmd = SPI_FLASH_READ_STATUS1_CMD;
 	err = spi_flash_read_status(spi, &spi_cfg, cmd, &spi_status);
 	if (err) {
-		printk("Could not read SPI flash status register %d. error = %d\n",
+		LOG_ERR("Could not read SPI flash status register %d. error = %d",
 			spi_status, err);
 		return;
 	}
 
-	printf("SPI flash status1 = 0x%02x\n", spi_status);
+	LOG_INF("SPI flash status1 = 0x%02x", spi_status);
 
-	printf("Send SPI flash write enable\n");
+	LOG_INF("Send SPI flash write enable");
 	cmd = SPI_FLASH_WRITE_ENABLE_CMD;
 	err = spi_flash_send_cmd(spi, &spi_cfg, cmd);
 	if (err) {
-		printk("Send SPI flash command 0x%02x failed error %d\n", cmd, err);
+		LOG_ERR("Send SPI flash command 0x%02x failed error %d", cmd, err);
 		return;
 	}
 
-	printf("Read SPI flash status1\n");
+	LOG_INF("Read SPI flash status1");
 	spi_status = 0x55u;
 	cmd = SPI_FLASH_READ_STATUS1_CMD;
 	err = spi_flash_read_status(spi, &spi_cfg, cmd, &spi_status);
 	if (err) {
-		printk("Could not read SPI flash status register %d. error = %d\n",
+		LOG_ERR("Could not read SPI flash status register %d. error = %d",
 			spi_status, err);
 		return;
 	}
 
-	printf("SPI flash status1 = 0x%02x\n", spi_status);
+	LOG_INF("SPI flash status1 = 0x%02x", spi_status);
 
-	printf("Send SPI flash write disable\n");
+	LOG_INF("Send SPI flash write disable");
 	cmd = SPI_FLASH_WRITE_DISABLE_CMD;
 	err = spi_flash_send_cmd(spi, &spi_cfg, cmd);
 	if (err) {
-		printk("Send SPI flash command 0x%02x failed error %d\n", cmd, err);
+		LOG_ERR("Send SPI flash command 0x%02x failed error %d", cmd, err);
 		return;
 	}
 
-	printf("Read SPI flash status1\n");
+	LOG_INF("Read SPI flash status1");
 	spi_status = 0x55u;
 	cmd = SPI_FLASH_READ_STATUS1_CMD;
 	err = spi_flash_read_status(spi, &spi_cfg, cmd, &spi_status);
 	if (err) {
-		printk("Could not read SPI flash status register %d. error = %d\n",
+		LOG_ERR("Could not read SPI flash status register %d. error = %d",
 			spi_status, err);
 		return;
 	}
 
-	printf("SPI flash status1 = 0x%02x\n", spi_status);
+	LOG_INF("SPI flash status1 = 0x%02x", spi_status);
 
-	memset(testbuf1, 0xAAu, sizeof(testbuf1));
-	spi_addr = 0u;
-	datalen = 16U;
-	cmd = SPI_FLASH_READ_SLOW_CMD;
-	printf("Read %u bytes at SPI offset 0x%08x using cmd 0x%02x\n", datalen, spi_addr, cmd);
-
-	err = spi_flash_read(spi, &spi_cfg, cmd, spi_addr, testbuf1, datalen);
+	LOG_INF("Erase 4KB sector containing address 0x%08x", SPI_FLASH_TEST_ADDR);
+	spi_addr = SPI_FLASH_TEST_ADDR & ~(SPI_FLASH_SECTOR_SIZE - 1u);
+	err = erase_region(spi, &spi_cfg, 0x20u, spi_addr);
 	if (err) {
-		printk("SPI flash read: error=%d\n", err);
+		LOG_ERR("Erase 4KB sector error %d", err);
 		return;
 	}
 
-	pr_data_buf(testbuf1, datalen);
+	LOG_INF("Read sector and check if erased");
+	memset(testbuf1, 0xAAu, sizeof(testbuf1));
+
+	spi_addr = SPI_FLASH_TEST_ADDR & ~(SPI_FLASH_SECTOR_SIZE - 1u);
+	datalen = SPI_FLASH_SECTOR_SIZE;
+	cmd = SPI_FLASH_READ_SLOW_CMD;
+	err = spi_flash_read(spi, &spi_cfg, cmd, spi_addr, testbuf1, datalen);
+	if (err) {
+		LOG_ERR("SPI flash read: cmd=0x%02x SPIAddr=0x%08x error=%d",
+			cmd, spi_addr, err);
+		return;
+	}
+
+	if (buf_contains(testbuf1, datalen, 0xffu)) {
+		LOG_INF("SPI flash region is erase");
+	} else {
+		LOG_ERR("SPI flash region not erased!");
+		return;
+	}
+
+	/* data to program */
+	for (n = 0; n < SPI_FLASH_PAGE_SIZE; n++) {
+		testbuf1[n] = n;
+	}
+
+	spi_addr = SPI_FLASH_TEST_ADDR;
+	datalen = SPI_FLASH_PAGE_SIZE;
+	LOG_INF("Program page at 0x%08x", spi_addr);
+
+	err = program_test_data(spi, &spi_cfg, spi_addr, testbuf1, datalen);
+	if (err) {
+		LOG_ERR("Program test data at 0x%08x len = %u error %d",
+			spi_addr, datalen, err);
+       		return;
+	}
+
+	LOG_INF("Read back page at 0x%08x", spi_addr);
+        memset(testbuf1, 0xAAu, sizeof(testbuf1));
+
+	cmd = SPI_FLASH_READ_SLOW_CMD;
+	err = spi_flash_read(spi, &spi_cfg, cmd, spi_addr, testbuf1, datalen);
+	if (err) {
+		LOG_ERR("SPI flash read: cmd=0x%02x SPIAddr=0x%08x error=%d",
+			cmd, spi_addr, err);
+		return;
+	}
 
 	for (n = 0; n < datalen; n++) {
-		if (testbuf1[n] != (n + 1)) {
+		if (testbuf1[n] != n) {
 			break;
 		}
 	}
-
 	if (n == datalen) {
-		printf("Data pattern matched expected value\n");
+		LOG_INF("After programming data pattern matched expected values");
 	} else {
-		printf("Data pattern does not match. Erase and program\n");
-		/* erase 4KB sector */
-
-		err = erase_region(spi, &spi_cfg, 0x20u, spi_addr);
-		if (err) {
-			printk("Erase 4KB sector at 0x%08x error %d\n", spi_addr, err);
-			return;
-		}
-
-		printf("Read back and check if ersased\n");
-
-		memset(testbuf1, 0xAAu, sizeof(testbuf1));
-
-		cmd = SPI_FLASH_READ_SLOW_CMD;
-		err = spi_flash_read(spi, &spi_cfg, cmd, spi_addr, testbuf1, datalen);
-		if (err) {
-			printk("SPI flash read: cmd=0x%02x SPIAddr=0x%08x error=%d\n",
-				cmd, spi_addr, err);
-			return;
-		}
-
-		pr_data_buf(testbuf1, datalen);
-
-		if (buf_contains(testbuf1, datalen, 0xffu)) {
-			printf("SPI flash region is erase\n");
-		} else {
-			printk("SPI flash region not erased!\n");
-			return;
-		}
-
-		/* data to program */
-		for (n = 0; n < datalen; n++) {
-			testbuf1[n] = n + 1;
-		}
-
-		/* program page at start of 4KB sector */
-		err = program_test_data(spi, &spi_cfg, spi_addr, testbuf1, datalen);
-		if (err) {
-			printk("Program test data at 0x%08x len = %u error %d\n",
-				spi_addr, datalen, err);
-			return;
-		}
-
-		memset(testbuf1, 0xAAu, sizeof(testbuf1));
-
-		cmd = SPI_FLASH_READ_SLOW_CMD;
-		err = spi_flash_read(spi, &spi_cfg, cmd, spi_addr, testbuf1, datalen);
-		if (err) {
-			printk("SPI flash read: cmd=0x%02x SPIAddr=0x%08x error=%d\n",
-				cmd, spi_addr, err);
-			return;
-		}
-
-		pr_data_buf(testbuf1, datalen);
-
-		for (n = 0; n < datalen; n++) {
-			if (testbuf1[n] != (n + 1)) {
-				break;
-			}
-		}
-		if (n == datalen) {
-			printf("After programming data pattern matched expected values\n");
-		} else {
-			printk("After programming data mismatch\n");
-			return;
-		}
+		LOG_ERR("After programming data mismatch");
+		return;
 	}
 
-	printf("Read data using fast read command\n");
+	LOG_INF("Read data using fast read command");
 	memset(testbuf1, 0x66u, sizeof(testbuf1));
 
 	cmd = SPI_FLASH_READ_FAST_CMD;
 	err = spi_flash_read(spi, &spi_cfg, cmd, spi_addr, testbuf1, datalen);
 	if (err) {
-		printk("SPI flash read: cmd=0x%02x SPIAddr=0x%08x error=%d\n",
+		LOG_ERR("SPI flash read: cmd=0x%02x SPIAddr=0x%08x error=%d",
 			cmd, spi_addr, err);
 		return;
 	}
 
-	pr_data_buf(testbuf1, datalen);
-
 	for (n = 0; n < datalen; n++) {
-		if (testbuf1[n] != (n + 1)) {
+		if (testbuf1[n] != n) {
 			break;
 		}
 	}
 	if (n == datalen) {
-		printf("SPI Fast Read: data pattern matched expected values\n");
+		LOG_INF("SPI Fast Read: data pattern matched expected values");
 	} else {
-		printk("SPI Fast Read: data mismatch\n");
+		LOG_ERR("SPI Fast Read: data mismatch");
 		return;
 	}
 
 #ifdef CONFIG_SPI_ASYNC
-	printf("\nTest SPI Async\n");
+	LOG_INF("Test SPI Async");
 	memset(testbuf1, 0x66u, sizeof(testbuf1));
-	cmd = 0x03u;
-	datalen = 16u;
-	spi_addr = 0;
-	printf("Read %u bytes at SPI offset 0x%08x using cmd 0x%02x\n", datalen, spi_addr, cmd);
+
+	cmd = SPI_FLASH_READ_SLOW_CMD;
+	spi_addr = SPI_FLASH_TEST_ADDR;
+	datalen = SPI_FLASH_PAGE_SIZE;
+	LOG_INF("Read %u bytes at SPI offset 0x%08x using cmd 0x%02x", datalen, spi_addr, cmd);
 
 	err = spi_flash_read_async(spi, &spi_cfg, cmd, spi_addr, testbuf1, datalen);
 	if (err) {
-		printf("SPI flash read async error %d\n", err);
+		LOG_ERR("SPI flash read async error %d", err);
 	}
 
-	pr_data_buf(testbuf1, datalen);
-
 	for (n = 0; n < datalen; n++) {
-		if (testbuf1[n] != (n + 1)) {
+		if (testbuf1[n] != n) {
 			break;
 		}
 	}
 	if (n == datalen) {
-		printf("SPI Aynsc Read: data pattern matched expected values\n");
+		LOG_INF("SPI Aynsc Read: data pattern matched expected values");
 	} else {
-		printk("SPI Async Read: data mismatch\n");
+		LOG_ERR("SPI Async Read: data mismatch");
 		return;
 	}
 #endif
 
-#ifdef CONFIG_SPI_EXTENDED_MODES
-	spi_addr = 0u;
-	datalen = 16u;
-	printf("\nTest GPSPI dual data read. GPSPI transmits using one line only. It can receive on one or two lines\n");
-	memset(testbuf1, 0xAAu, sizeof(testbuf1));
-	cmd = SPI_FLASH_READ_DUAL_CMD;
-	printf("Read %u bytes at SPI offset 0x%08x using dual (1-1-2-8)\n", datalen, spi_addr);
-
-	err = spi_flash_read_dual(spi, &spi_cfg, cmd, spi_addr, testbuf1, datalen);
-	if (err) {
-		printk("SPI flash read: cmd=0x%02x SPIAddr=0x%08x error=%d\n", cmd, spi_addr, err);
-		return;
-	}
-
-	pr_data_buf(testbuf1, datalen);
-
-	for (n = 0; n < datalen; n++) {
-		if (testbuf1[n] != (n + 1)) {
-			break;
-		}
-	}
-	if (n == datalen) {
-		printf("SPI read dual opcode 0x3b matched expected value\n");
-	} else {
-		printf("SPI read dual opcode 0x3b data MISMATCH\n");
-	}
-#endif
-
-	printf("end main\n");
+	LOG_INF("end main");
 }
