2c3dc541ee8d3fa04efdf46aa85f93f016181ffc
scott worley <scott.worley@microchip.com>
2023-03-30 11:46:19 -0400

    drivers: pinctrl: Microchip XEC PINCTRL glitch fix
    Glitches were observed if a GPIO pin was configured by
    ROM to a non-default state and then Zephyr PINCTRL
    reconfigured the pin. The fix involves using the correct
    PINCTRL YAML output enable and state flags. Reading the
    current spin state and reflecting into new pin configuration
    if the pin is output and the drive low/high properties are
    not present. We also take advantage of GPIO hardware reflecing
    the alternate output value in the parallel output bit before
    enabling parallel output mode. Interpret boolean flags with
    both enable and disable as do not touch if neither flag is
    present. We give precedence to enable over disable if both
    flags mistakenly appear.
    
    Signed-off-by: scott worley <scott.worley@microchip.com>

--- soc/arm/microchip_mec/common/pinctrl_soc.h
+++ soc/arm/microchip_mec/common/pinctrl_soc.h
@@ -26,74 +26,47 @@ extern "C" {
 /** @cond INTERNAL_HIDDEN */
 
 /* Type for MCHP XEC pin. */
-typedef struct {
-	/** Pinmux settings (port, pin and function). */
-	uint16_t pinmux;
-	/** Pin configuration (bias, drive and slew rate). */
-	uint16_t pincfg;
+typedef struct pinctrl_soc_pin {
+	uint32_t pinmux;
 } pinctrl_soc_pin_t;
 
 /* initialize pinmux member fields of pinctrl_pin_t */
 #define Z_PINCTRL_MCHP_XEC_PINMUX_INIT(node_id) DT_PROP(node_id, pinmux)
 
-#define MCHP_XEC_BIAS_DIS_VAL(nid)	\
-	(MCHP_XEC_NO_PULL * DT_PROP(nid, bias_disable))
+#define MCHP_XEC_BIAS_DIS_VAL(nid) (1u * DT_PROP_OR(nid, bias_disable, 0))
+#define MCHP_XEC_BIAS_PD_VAL(nid) (1u * DT_PROP_OR(nid, bias_pull_down, 0))
+#define MCHP_XEC_BIAS_PU_VAL(nid) (1u * DT_PROP_OR(nid, bias_pull_up, 0))
+#define MCHP_XEC_DRV_PP_VAL(nid) (1u * DT_PROP_OR(nid, drive_push_pull, 0))
+#define MCHP_XEC_DRV_OD_VAL(nid) (1u * DT_PROP_OR(nid, drive_open_drain, 0))
+#define MCHP_XEC_OUT_DIS_VAL(nid) (1u * DT_PROP_OR(nid, output_disable, 0))
+#define MCHP_XEC_OUT_EN_VAL(nid) (1u * DT_PROP_OR(nid, output_enable, 0))
+#define MCHP_XEC_OUT_DRV_HI(nid) (1u * DT_PROP_OR(nid, output_high, 0))
+#define MCHP_XEC_OUT_DRV_LO(nid) (1u * DT_PROP_OR(nid, output_low, 0))
+#define MCHP_XEC_PAD_INPUT_DIS_VAL(nid)	(1u * DT_PROP_OR(nid, input_disable, 0))
+#define MCHP_XEC_SLEW_VAL(nid) (DT_ENUM_IDX_OR(nid, slew_rate, 0))
+#define MCHP_XEC_DRVSTR_VAL(nid) (DT_ENUM_IDX_OR(nid, drive_strength, 0))
+#define MCHP_XEC_LOW_POWER_EN(nid) (1u * DT_PROP_OR(nid, low_power_enable, 0))
+#define MCHP_XEC_FUNC_INVERT_VAL(nid) (1u * DT_PROP_OR(nid, microchip_output_func_invert, 0))
 
-#define MCHP_XEC_BIAS_PU_VAL(nid)	\
-	(MCHP_XEC_PULL_UP * DT_PROP(nid, bias_pull_up))
-
-#define MCHP_XEC_BIAS_PD_VAL(nid)	\
-	(MCHP_XEC_PULL_DOWN * DT_PROP(nid, bias_pull_down))
-
-#define MCHP_XEC_DRV_PP_VAL(nid)	\
-	(MCHP_XEC_PUSH_PULL * DT_PROP(nid, drive_push_pull))
-
-#define MCHP_XEC_DRV_OD_VAL(nid)	\
-	(MCHP_XEC_OPEN_DRAIN * DT_PROP(nid, drive_open_drain))
-
-#define MCHP_XEC_OVAL_DRV_LO(nid)	\
-	(MCHP_XEC_OVAL_LOW * DT_PROP(nid, output_low))
-
-#define MCHP_XEC_OVAL_DRV_HI(nid)	\
-	(MCHP_XEC_OVAL_HIGH * DT_PROP(nid, output_high))
-
-#define MCHP_XEC_LOW_POWER_EN(nid)	\
-	(MCHP_XEC_PIN_LOW_POWER * DT_PROP(nid, low_power_enable))
-
-#define MCHP_XEC_SLEW_VAL(nid)		\
-	(DT_ENUM_IDX_OR(nid, slew_rate, MCHP_XEC_OSPEEDR_NO_CHG) << \
-		MCHP_XEC_OSPEEDR_POS)
-
-#define MCHP_XEC_DRVSTR_VAL(nid)	\
-	(DT_ENUM_IDX_OR(nid, drive_strength, MCHP_XEC_ODRVSTR_NO_CHG) << \
-		MCHP_XEC_ODRVSTR_POS)
-
-#define MCHP_XEC_FUNC_INVERT_VAL(nid)	\
-	(MCHP_XEC_FUNC_INVERT * DT_PROP(nid, microchip_output_func_invert))
-
-/* initialize pincfg field in structure pinctrl_pin_t */
-#define Z_PINCTRL_MCHP_XEC_PINCFG_INIT(node_id)				\
-	((MCHP_XEC_BIAS_DIS_VAL(node_id) << MCHP_XEC_PUPDR_POS) |	\
-	 (MCHP_XEC_BIAS_PU_VAL(node_id) << MCHP_XEC_PUPDR_POS) |	\
-	 (MCHP_XEC_BIAS_PD_VAL(node_id) << MCHP_XEC_PUPDR_POS) |	\
-	 (MCHP_XEC_DRV_PP_VAL(node_id) << MCHP_XEC_OTYPER_POS) |	\
-	 (MCHP_XEC_DRV_OD_VAL(node_id) << MCHP_XEC_OTYPER_POS) |	\
-	 (MCHP_XEC_OVAL_DRV_LO(node_id) << MCHP_XEC_OVAL_POS) |		\
-	 (MCHP_XEC_OVAL_DRV_HI(node_id) << MCHP_XEC_OVAL_POS) |		\
-	 (MCHP_XEC_LOW_POWER_EN(node_id) << MCHP_XEC_PIN_LOW_POWER_POS)	| \
-	 (MCHP_XEC_FUNC_INVERT_VAL(node_id) << MCHP_XEC_FUNC_INV_POS)	| \
-	 MCHP_XEC_SLEW_VAL(node_id) |					\
-	 MCHP_XEC_DRVSTR_VAL(node_id))
-
-/* initialize pin structure members */
-#define Z_PINCTRL_STATE_PIN_INIT(node_id, state_prop, idx)		\
-	{ .pinmux = Z_PINCTRL_MCHP_XEC_PINMUX_INIT(			\
-		DT_PROP_BY_IDX(node_id, state_prop, idx)),		\
-	  .pincfg = Z_PINCTRL_MCHP_XEC_PINCFG_INIT(			\
-		DT_PROP_BY_IDX(node_id, state_prop, idx)), },
+#define Z_PINCTRL_STATE_PIN_INIT(node_id, state_prop, idx)					\
+	{ .pinmux = (Z_PINCTRL_MCHP_XEC_PINMUX_INIT(DT_PROP_BY_IDX(node_id, state_prop, idx))	\
+		     | (MCHP_XEC_BIAS_DIS_VAL(node_id) << MCHP_XEC_NO_PUD_POS)			\
+		     | (MCHP_XEC_BIAS_PD_VAL(node_id) << MCHP_XEC_PD_POS)			\
+		     | (MCHP_XEC_BIAS_PU_VAL(node_id) << MCHP_XEC_PU_POS)			\
+		     | (MCHP_XEC_DRV_PP_VAL(node_id) << MCHP_XEC_PUSH_PULL_POS)			\
+		     | (MCHP_XEC_DRV_OD_VAL(node_id) << MCHP_XEC_OPEN_DRAIN_POS)		\
+		     | (MCHP_XEC_OUT_DIS_VAL(node_id) << MCHP_XEC_OUT_DIS_POS)			\
+		     | (MCHP_XEC_OUT_EN_VAL(node_id) << MCHP_XEC_OUT_EN_POS)			\
+		     | (MCHP_XEC_OUT_DRV_HI(node_id) << MCHP_XEC_OUT_HI_POS)			\
+		     | (MCHP_XEC_OUT_DRV_LO(node_id) << MCHP_XEC_OUT_LO_POS)			\
+		     | (MCHP_XEC_PAD_INPUT_DIS_VAL(node_id) << MCHP_XEC_INPUT_PAD_DIS_POS)	\
+		     | (MCHP_XEC_SLEW_VAL(node_id) << MCHP_XEC_SLEW_RATE_POS)			\
+		     | (MCHP_XEC_DRVSTR_VAL(node_id) << MCHP_XEC_DRV_STR_POS)			\
+		     | (MCHP_XEC_LOW_POWER_EN(node_id) << MCHP_XEC_PIN_LOW_POWER_POS)		\
+		     | (MCHP_XEC_FUNC_INVERT_VAL(node_id) << MCHP_XEC_FUNC_INV_POS) ) },
 
 /* Use DT FOREACH macro to initialize each used pin */
-#define Z_PINCTRL_STATE_PINS_INIT(node_id, prop)			\
+#define Z_PINCTRL_STATE_PINS_INIT(node_id, prop)					\
 	{DT_FOREACH_PROP_ELEM(node_id, prop, Z_PINCTRL_STATE_PIN_INIT)}
 
 /** @endcond */
