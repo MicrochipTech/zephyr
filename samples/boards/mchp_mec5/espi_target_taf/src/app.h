963713272c406a9850b2bb500afb17b4ec4a6b6a
Scott Worley <scott.worley@microchip.com>
2024-12-13 16:34:14 -0500

    samples: boards: mchp_mec5: espi_target_taf: Add RPMC support
    We add support for HW RPMC flash devices and code to create the
    signed command packets. EVB has two W25R256JV flashed connected
    to QSPI CS0 and CS1.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

---
+++ samples/boards/mchp_mec5/espi_target_taf/src/app.h
@@ -0,0 +1,14 @@
+
+/* Copyright (c) 2024 Microchip Technology Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __APP_H__
+#define __APP_H__
+
+#include <stddef.h>
+#include <stdint.h>
+
+uint32_t app_flash_jedec_id(uint8_t cs);
+
+#endif /* __APP_H__ */
