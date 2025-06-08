116b24b36a546a0bdc3abb4cef5ef8326ab9b423
Scott Worley <scott.worley@microchip.com>
2025-06-08 17:10:25 -0400

    drivers: espi: microchip: New eSPI driver for Microchip MEC174x/5x
    WIP: work-in-progress
    Next generation Zephyr eSPI driver for Microchip MEC174x/5x and
    future SoCs. Driver functionality is broken into multiple files
    based on the eSPI channel. ESPI next generation driver performs
    data transport.  Data interpretation will be handed by a new
    eSPI subystem. Different subsystem may be implements based on
    platform and Host OS requirements.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

---
+++ drivers/espi/espi_mchp_mec5.c
@@ -0,0 +1,200 @@
+/*
+ * Copyright (c) 2025 Microchip Technology Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT microchip_mec5_espi
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(espi, CONFIG_ESPI_LOG_LEVEL);
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/pinctrl.h>
+#include <zephyr/drivers/espi.h>
+#include <zephyr/dt-bindings/espi/mchp_mec5_espi.h>
+#include <zephyr/pm/device.h>
+#include <zephyr/sys/sys_io.h>
+#include <zephyr/sys/util.h>
+#include <soc.h>
+#include <zephyr/irq.h>
+
+/* MEC5 HAL */
+#include <device_mec5.h>
+#include <mec_ecia_api.h>
+#include <mec_espi_api.h>
+
+/* local */
+#include "espi_utils.h"
+#include "espi_mchp_mec5.h"
+#include "mchp/espi_mchp_mec5_regs.h"
+
+/* If SoC was configured for Boot-ROM to load Zephyr application from
+ * eSPI CAF then the eSPI link is up and Host has enabled VW channel.
+ */
+static bool is_espi_bootrom_config(const struct device *dev)
+{
+	const struct espi_mec5_drv_cfg *drvcfg = dev->config;
+	mm_reg_t iob = (mm_reg_t)drvcfg->ioc_base;
+
+	if (((sys_read8(iob + ESPI_ACTV) & BIT(ESPI_ACTV_EN_POS)) != 0) &&
+	    ((sys_read32(iob + ESPI_VWC_TAF) & BIT(ESPI_VW_RDY_POS)) != 0)) {
+		return true;
+	}
+
+	return false;
+}
+
+/* eSPI Reset interrupt handler */
+static void espi_mec5_ereset_isr(const struct device *dev)
+{
+	/* TODO */
+}
+
+/* -------- Public API -------- */
+static int espi_mec5_config_api(const struct device *dev, struct espi_cfg *cfg)
+{
+	return -ENOTSUP;
+}
+
+static bool espi_mec5_get_chan_status_api(const struct device *dev, enum espi_channel ch)
+{
+	return -ENOTSUP;
+}
+
+static int espi_mec5_manage_cb_api(const struct device *dev, struct espi_callback *callback,
+				   bool set)
+{
+	return -ENOTSUP;
+}
+
+static int espi_mec5_driver_init(const struct device *dev)
+{
+	const struct espi_mec5_drv_cfg *drvcfg = dev->config;
+	struct espi_mec5_drv_data *data = dev->data;
+	int ret = 0;
+
+	data->dev = dev;
+	data->status = 0;
+
+	ret = pinctrl_apply_state(drvcfg->pcfg, PINCTRL_STATE_DEFAULT);
+	if (ret != 0) {
+		return ret;
+	}
+
+	if (is_espi_bootrom_config(dev)) {
+		return 0;
+	}
+
+#ifdef CONFIG_ESPI_VWIRE_CHANNEL
+	ret = espi_mec5_init_vwires(dev);
+	if (ret != 0) {
+		return ret;
+	}
+#endif
+
+	if (drvcfg->irq_config) {
+		drvcfg->irq_config(dev);
+	}
+
+	return 0;
+}
+
+#if CONFIG_PM_DEVICE
+static int espi_mec5_pm_action(const struct device *dev, enum pm_device_action action)
+{
+	/* TODO PM_DEVICE_ACTION_xxx */
+	return 0;
+}
+#endif /* CONFIG_PM_DEVICE */
+
+static DEVICE_API(espi, espi_mec5_driver_api) = {
+	.config = espi_mec5_config_api,
+	.get_channel_status = espi_mec5_get_chan_status_api,
+#ifdef CONFIG_ESPI_PERIPHERAL_CHANNEL
+	.read_request = espi_mec5_read_req_api,
+	.write_request = espi_mec5_write_req_api,
+	.read_lpc_request = espi_mec5_read_lpc_req_api,
+	.write_lpc_request = espi_mec5_write_lpc_req_api,
+#endif
+#ifdef CONFIG_ESPI_VWIRE_CHANNEL
+	.send_vwire = espi_mec5_send_vwire_api,
+	.receive_vwire = espi_mec5_recv_vwire_api,
+#endif
+#ifdef CONFIG_ESPI_OOB_CHANNEL
+	.send_oob = espi_mec5_send_oob_api,
+	.receive_oob = espi_mec5_recv_oob_api,
+#endif
+#ifdef CONFIG_ESPI_FLASH_CHANNEL
+	.flash_read = espi_mec5_flash_read_api,
+	.flash_write = espi_mec5_flash_write_api,
+	.flash_erase = espi_mec5_flash_erase_api,
+#endif
+	.manage_callback = espi_mec5_manage_cb_api,
+};
+
+#define MEC5_ESPI_IRQ_CONNECT(inst, dev)                                                           \
+	IRQ_CONNECT(DT_INST_IRQ_BY_NAME(inst, erst, irq),                                          \
+		    DT_INST_IRQ_BY_NAME(inst, erst, priority), espi_mec5_ereset_isr,               \
+		    DEVICE_DT_INST_GET(inst), 0);                                                  \
+	irq_enable(DT_INST_IRQ_BY_NAME(inst, erst, irq));                                          \
+	sys_write32(BIT(ESPI_GIRQ_ERST_POS), ESPI_GIRQ_ENSET_ADDR);
+
+#ifdef CONFIG_ESPI_VWIRE_CHANNEL
+#define MEC5_ESPI_VW_IRQ_CONNECT(dev) espi_mec5_vw_irq_connect(dev);
+#else
+#define MEC5_ESPI_VW_IRQ_CONNECT(dev)
+#endif /* CONFIG_ESPI_VWIRE_CHANNEL */
+
+#ifdef CONFIG_ESPI_PERIPHERAL_CHANNEL
+#define MEC5_ESPI_PC_IRQ_CONNECT(dev) espi_mec5_pc_irq_connect(dev);
+#else
+#define MEC5_ESPI_PC_IRQ_CONNECT(dev)
+#endif /* CONFIG_ESPI_PERIPHERAL_CHANNEL */
+
+#ifdef CONFIG_ESPI_OOB_CHANNEL
+#define MEC5_ESPI_OOB_IRQ_CONNECT(dev) espi_mec5_oob_irq_connect(dev)
+#define MEC5_ESPI_OOB_DATA(inst) \
+	static uint8_t espi_mec5_oob_rx_buf_##inst[CONFIG_ESPI_OOB_BUFFER_SIZE] __aligned(4)
+#define MEC5_ESPI_OOB_DATA_INIT(inst) .oob_rxb = espi_mec5_oob_rx_buf_##inst,
+#define MEC5_ESPI_OOB_DCFG(inst) .oob_rxb_size = sizeof(espi_mec5_oob_rx_buf_##inst),
+#else
+#define MEC5_ESPI_OOB_IRQ_CONNECT(dev)
+#define MEC5_ESPI_OOB_DATA(inst)
+#define MEC5_ESPI_OOB_DATA_INIT(inst)
+#define MEC5_ESPI_OOB_DCFG(inst)
+#endif /* CONFIG_ESPI_PERIPHERAL_CHANNEL */
+
+#ifdef CONFIG_ESPI_FLASH_CHANNEL
+#define MEC5_ESPI_FLASH_IRQ_CONNECT(dev) espi_mec5_fc_irq_connect(dev)
+#else
+#define MEC5_ESPI_FLASH_IRQ_CONNECT(dev)
+#endif /* CONFIG_ESPI_PERIPHERAL_CHANNEL */
+
+#define MEC5_ESPI_DEVICE(inst)                                                                     \
+	PINCTRL_DT_INST_DEFINE(inst);                                                              \
+	static void espi_mec5_irq_config_##inst(const struct device *dev)                          \
+	{                                                                                          \
+		MEC5_ESPI_IRQ_CONNECT(inst, dev);                                                  \
+		MEC5_ESPI_VW_IRQ_CONNECT(dev);                                                     \
+		MEC5_ESPI_PC_IRQ_CONNECT(dev);                                                     \
+		MEC5_ESPI_OOB_IRQ_CONNECT(dev);                                                    \
+		MEC5_ESPI_FLASH_IRQ_CONNECT(dev);                                                  \
+	}                                                                                          \
+	MEC5_ESPI_OOB_DATA(inst);                                                                  \
+	struct espi_mec5_drv_data espi_mec5_drv_data_##inst = {                                    \
+		MEC5_ESPI_OOB_DATA_INIT(inst)                                                      \
+	};                                                                                         \
+	struct espi_mec5_drv_cfg espi_mec5_dcfg_##inst = {                                         \
+		.ioc_base = DT_INST_REG_ADDR_BY_NAME(inst, io),                                    \
+		.memc_base = DT_INST_REG_ADDR_BY_NAME(inst, mem),                                  \
+		.vwc_base = DT_INST_REG_ADDR_BY_NAME(inst, vw),                                    \
+		.irq_config = espi_mec5_irq_config_##inst,                                         \
+		.pcfg = PINCTRL_DT_INST_DEV_CONFIG_GET(inst),                                      \
+		MEC5_ESPI_OOB_DCFG(inst)                                                           \
+	};                                                                                         \
+	PM_DEVICE_DT_INST_DEFINE(inst, espi_mec5_pm_action);                                       \
+	DEVICE_DT_INST_DEFINE(inst, &espi_mec5_driver_init, PM_DEVICE_DT_INST_GET(inst),           \
+			      &espi_mec5_drv_data_##inst, &espi_mec5_dcfg_##inst, POST_KERNEL,     \
+			      CONFIG_ESPI_INIT_PRIORITY, &espi_mec5_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(MEC5_ESPI_DEVICE)
---
+++ drivers/espi/espi_mchp_mec5.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2025 Microchip Technology Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_ESPI_MCHP_MEC5_ESPI_H_
+#define ZEPHYR_DRIVERS_ESPI_MCHP_MEC5_ESPI_H_
+
+#include <zephyr/device.h>
+#include <zephyr/kernel.h>
+#include <zephyr/types.h>
+#include <zephyr/drivers/espi.h>
+#include <zephyr/drivers/pinctrl.h>
+
+struct espi_mec5_drv_cfg {
+	uintptr_t ioc_base;
+	uintptr_t memc_base;
+	uintptr_t vwc_base;
+	void (*irq_config)(const struct device *dev);
+	const struct pinctrl_dev_config *pcfg;
+	uint8_t freq_id;
+	uint8_t chan_msk;
+#if defined(CONFIG_ESPI_OOB_CHANNEL)
+	uint16_t oob_rxb_size;
+#endif
+};
+
+struct espi_mec5_drv_data {
+	const struct device *dev;
+	volatile uint32_t status;
+	struct espi_callback vwcb;
+	sys_slist_t callbacks;
+#if defined(CONFIG_ESPI_OOB_CHANNEL)
+	struct k_sem oob_tx_sync;
+	struct k_sem oob_rx_sync;
+	uint8_t *oob_rxb;
+	uint16_t oob_rx_len;
+#endif
+};
+
+#if defined(CONFIG_ESPI_PERIPHERAL_CHANNEL)
+int espi_mec5_pc_pltrst_handler(const struct device *dev, uint8_t pltrst_state);
+void espi_mec5_pc_irq_connect(const struct device *dev);
+
+int espi_mec5_read_req_api(const struct device *dev, struct espi_request_packet *req);
+
+int espi_mec5_write_req_api(const struct device *dev, struct espi_request_packet *req);
+
+int espi_mec5_read_lpc_req_api(const struct device *dev, enum lpc_peripheral_opcode op,
+				uint32_t *data);
+
+int espi_mec5_write_lpc_req_api(const struct device *dev, enum lpc_peripheral_opcode op,
+				uint32_t *data);
+#endif
+
+#if defined(CONFIG_ESPI_VWIRE_CHANNEL)
+int espi_mec5_init_vwires(const struct device *dev);
+void espi_mec5_vw_irq_connect(const struct device *dev);
+
+int espi_mec5_send_vwire_api(const struct device *dev, enum espi_vwire_signal vw, uint8_t level);
+int espi_mec5_recv_vwire_api(const struct device *dev, enum espi_vwire_signal vw, uint8_t *level);
+#endif
+
+#if defined(CONFIG_ESPI_OOB_CHANNEL)
+void espi_mec5_oob_irq_connect(const struct device *dev);
+
+int espi_mec5_send_oob_api(const struct device *dev, struct espi_oob_packet *pckt);
+int espi_mec5_recv_oob_api(const struct device *dev, struct espi_oob_packet *pckt);
+#endif
+
+#if defined(CONFIG_ESPI_FLASH_CHANNEL)
+void espi_mec5_fc_irq_connect(const struct device *dev);
+
+/* eSPI driver flash channel API implementation */
+int espi_mec5_flash_read_api(const struct device *dev, struct espi_flash_packet *pckt);
+int espi_mec5_flash_write_api(const struct device *dev, struct espi_flash_packet *pckt);
+int espi_mec5_flash_erase_api(const struct device *dev, struct espi_flash_packet *pckt);
+#endif
+
+#endif /*  ZEPHYR_DRIVERS_ESPI_MCHP_MEC5_ESPI_H_ */
