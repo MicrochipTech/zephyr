b3a0ed6335814367d7ea94d7c18d6de2886bbd89
Scott Worley <scott.worley@microchip.com>
2025-07-25 17:21:41 -0400

    samples: boards: microchip: mmcr_access: Add 16-bit test
    Added a 16-bit walking one test.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

--- samples/boards/microchip/mmcr_access/src/main.c
+++ samples/boards/microchip/mmcr_access/src/main.c
@@ -22,6 +22,7 @@ LOG_MODULE_DECLARE(app, CONFIG_LOG_DEFAULT_LEVEL);
 int main(void)
 {
 	mem_addr_t maddr = MEC_VB_MEM_BASE_ADDR;
+	uint16_t va1 = 0;
 	uint8_t val = 0xA5u;
 	uint8_t val2 = 0;
 
@@ -32,6 +33,31 @@ int main(void)
 
 	LOG_INF("8-bit Read [0x%0lx] = 0x%02x", maddr, val2);
 
+	soc_set_bit8(maddr, BIT(1));
+	val2 = sys_read8(maddr);
+	if ((val2 & BIT(1)) == 0) {
+		LOG_ERR("Failed to set BIT(1): result = 0x%02x", val2);
+	}
+
+	soc_clear_bit8(maddr, BIT(7));
+	val2 = sys_read8(maddr);
+	if ((val2 & BIT(7)) != 0) {
+		LOG_ERR("Failed to clear BIT(7): result = 0x%02x", val2);
+	}
+
+	maddr += 2u;
+	sys_write16(0, maddr);
+	va1 = sys_read16(maddr);
+	LOG_INF("Write 16-bit 0 to [%0lx]", maddr);
+
+	for (uint32_t n = 0; n < 16; n++) {
+		soc_set_bit16(maddr, n);
+		if (soc_test_bit16(maddr, n) == 0) {
+			LOG_ERR("16-bit set bit %u", n);
+		}
+	}
+
 	printf("\nProgram End\n");
+
 	return 0;
 }
