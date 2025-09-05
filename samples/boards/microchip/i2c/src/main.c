c04bcba7acbc44e59b1d1b8564a0ef3aa17b21f7
Scott Worley <scott.worley@microchip.com>
2025-09-05 14:43:56 -0400

    samples: boards: microchip: i2c: Add read of PCA9555
    We add code to configure I2C controller 0 and perform
    an I2C write-read of the PCA9555.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

--- samples/boards/microchip/i2c/src/main.c
+++ samples/boards/microchip/i2c/src/main.c
@@ -16,12 +16,59 @@
 
 LOG_MODULE_REGISTER(app, CONFIG_LOG_DEFAULT_LEVEL);
 
+#define PCA9555_CMD_PORT0_IN  0
+#define PCA9555_CMD_PORT1_IN  1u
+#define PCA9555_CMD_PORT0_OUT 2u
+#define PCA9555_CMD_PORT1_OUT 3u
+#define PCA9555_CMD_PORT0_POL 4u
+#define PCA9555_CMD_PORT1_POL 5u
+#define PCA9555_CMD_PORT0_CFG 6u
+#define PCA9555_CMD_PORT1_CFG 7u
+
+#define PCA9555_I2C_ADDR DT_REG_ADDR(DT_NODELABEL(pca9555_evb))
+#define LTC2489_I2C_ADDR DT_REG_ADDR(DT_NODELABEL(ltc2489_evb))
+
 static const struct device *i2c0_dev = DEVICE_DT_GET(DT_ALIAS(i2c0));
+
+uint8_t i2c_wr_buf[64];
+uint8_t i2c_rd_buf[64];
 
 int main(void)
 {
+	uint32_t i2c0_config = 0;
+	uint16_t i2c_addr = 0;
+	size_t nread = 0, nwrite = 0;
+	int rc = 0;
+
 	if (!device_is_ready(i2c0_dev)) {
 		LOG_ERR("I2C 0 device [%s] not ready!", i2c0_dev->name);
+		return -1;
+	}
+
+	LOG_INF("Configure I2C 0 as controller at 100KHz");
+
+	i2c0_config = I2C_MODE_CONTROLLER | I2C_SPEED_SET(I2C_SPEED_STANDARD);
+
+	rc = i2c_configure(i2c0_dev, i2c0_config);
+	if (rc != 0) {
+		LOG_ERR("I2C 0 device config failed");
+		return -1;
+	}
+
+	LOG_INF("PCA9555 on I2C 0: Read the two data registers");
+
+	memset(i2c_rd_buf, 0x55, sizeof(i2c_rd_buf));
+
+	i2c_addr = (uint16_t)PCA9555_I2C_ADDR;
+	i2c_wr_buf[0] = PCA9555_CMD_PORT0_IN;
+
+	nread = 2u;
+	nwrite = 1u;
+
+	rc = i2c_write_read(i2c0_dev, i2c_addr, (const void *)i2c_wr_buf, nwrite,
+			    (void *)i2c_rd_buf, nread);
+	if (rc != 0) {
+		LOG_ERR("i2c_write_read error (%d)", rc);
 		return -1;
 	}
 
