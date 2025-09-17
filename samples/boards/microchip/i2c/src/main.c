a33117de07c4c742a9cf677ec7a06930d311b131
Scott Worley <scott.worley@microchip.com>
2025-09-17 16:25:52 -0400

    samples: boards: microchip: i2c: I2C target buffer mode updates
    We updates the sample application target buffer mode tests.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

--- samples/boards/microchip/i2c/src/main.c
+++ samples/boards/microchip/i2c/src/main.c
@@ -13,6 +13,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/i2c/mchp_xec_i2c.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(app, CONFIG_LOG_DEFAULT_LEVEL);
@@ -42,14 +43,17 @@ uint8_t i2c_rd_buf[64];
 #define I2C1_TARG_ADDR1 0x40u
 #define I2C1_TARG_ADDR2 0x41u
 
+#define TARGET_I2C_ADDR2_RX_BUF_SIZE 10
+#define TARGET_I2C_ADDR2_TX_BUF_SIZE 10
+
 uint32_t target_i2c_addr1_rx_idx;
 uint32_t target_i2c_addr1_tx_idx;
 uint32_t target_i2c_addr2_rx_idx;
 uint32_t target_i2c_addr2_tx_idx;
 uint8_t target_i2c_addr1_rx_buf[256];
 uint8_t target_i2c_addr1_tx_buf[256];
-uint8_t target_i2c_addr2_rx_buf[256];
-uint8_t target_i2c_addr2_tx_buf[256];
+uint8_t target_i2c_addr2_rx_buf[TARGET_I2C_ADDR2_RX_BUF_SIZE + 4u];
+uint8_t target_i2c_addr2_tx_buf[TARGET_I2C_ADDR2_TX_BUF_SIZE + 4u];
 
 uint8_t i2c0_tx_buf[256];
 uint8_t i2c0_rx_buf[256];
@@ -165,6 +169,9 @@ int main(void)
 	}
 
 	/* I2C0 write to I2C1 */
+	target_i2c_addr1_rx_idx = 0;
+	target_i2c_addr1_tx_idx = 0;
+
 	fill_buf(i2c0_tx_buf, 4u, 0x11u, 1u);
 
 	LOG_INF("Write 4 bytes to I2C target address 0x%0x", I2C1_TARG_ADDR1);
@@ -173,11 +180,24 @@ int main(void)
 	rc = i2c_write(i2c0_dev, i2c0_tx_buf, 4u, I2C1_TARG_ADDR1);
 	LOG_INF("I2C Write API returned (%d)", rc);
 
-	while (rc != 0) {
+	LOG_INF("I2C1 target RX index = 0x%0x", target_i2c_addr1_rx_idx);
+	LOG_INF("I2C1 target TX index = 0x%0x", target_i2c_addr1_tx_idx);
+
+	LOG_HEXDUMP_INF(target_i2c_addr1_rx_buf, 64u, "I2C1 target RX buffer");
+	LOG_HEXDUMP_INF(target_i2c_addr1_tx_buf, 64u, "I2C1 target TX buffer");
+#if 0
+	while (spin_val != 0) {
 		;
 	}
+#endif
+	i2c_mchp_xec_v3_debug_init(i2c1_dev);
+
+	spin_val = 0xDEADBEEFU;
 
 	/* I2C0 read from I2C1 */
+	target_i2c_addr1_rx_idx = 0;
+	target_i2c_addr1_tx_idx = 0;
+
 	fill_buf(i2c0_rx_buf, sizeof(i2c0_rx_buf), 0x55u, 0);
 	LOG_INF("Read 8 bytes from I2C target address 0x%0x", I2C1_TARG_ADDR1);
 
@@ -186,9 +206,71 @@ int main(void)
 		LOG_ERR("I2C Read API returned error (%d)", rc);
 	}
 
-	while (spin_val) {
+	LOG_HEXDUMP_INF(i2c0_rx_buf, 64u, "App RX buffer");
+
+	LOG_INF("I2C1 target RX index = 0x%0x", target_i2c_addr1_rx_idx);
+	LOG_INF("I2C1 target TX index = 0x%0x", target_i2c_addr1_tx_idx);
+
+	LOG_HEXDUMP_INF(target_i2c_addr1_rx_buf, 64u, "I2C1 target RX buffer");
+	LOG_HEXDUMP_INF(target_i2c_addr1_tx_buf, 64u, "I2C1 target TX buffer");
+
+#if 0
+	while (spin_val != 0) {
 		;
 	}
+#endif
+	i2c_mchp_xec_v3_debug_init(i2c1_dev);
+
+	spin_val = 0xDEADBEEFU;
+
+	LOG_INF("Write more than I2C1 target address 2 buffer size");
+	target_i2c_addr1_rx_idx = 0;
+	target_i2c_addr1_tx_idx = 0;
+
+	nwrite = TARGET_I2C_ADDR2_RX_BUF_SIZE + 2u;
+	fill_buf(i2c0_tx_buf, nwrite, 0x11u, 1u);
+	LOG_HEXDUMP_INF(i2c0_tx_buf, nwrite, "i2c0_tx_buf");
+
+	rc = i2c_write(i2c0_dev, i2c0_tx_buf, nwrite, I2C1_TARG_ADDR2);
+	LOG_INF("I2C Write API returned (%d)", rc);
+
+	LOG_INF("I2C1 target RX index = 0x%0x", target_i2c_addr2_rx_idx);
+	LOG_INF("I2C1 target TX index = 0x%0x", target_i2c_addr2_tx_idx);
+
+	LOG_HEXDUMP_INF(target_i2c_addr2_rx_buf, sizeof(target_i2c_addr2_rx_buf),
+			"I2C1 target addr2 RX buffer");
+	LOG_HEXDUMP_INF(target_i2c_addr2_tx_buf, sizeof(target_i2c_addr2_rx_buf),
+			"I2C1 target addr2 TX buffer");
+
+	LOG_INF("I2C0 write-read from I2C1");
+	memset(i2c0_tx_buf, 0x55u, sizeof(i2c0_tx_buf));
+	memset(i2c0_rx_buf, 0x66u, sizeof(i2c0_rx_buf));
+
+	fill_buf(target_i2c_addr1_rx_buf, 256, 0x99u, 0);
+	fill_buf(target_i2c_addr1_tx_buf, 256, 0, 1);
+
+	target_i2c_addr1_rx_idx = 0;
+	target_i2c_addr1_tx_idx = 0;
+
+	i2c_addr = I2C1_TARG_ADDR1;
+	nwrite = 2u;
+	nread = 32u;
+
+	i2c0_tx_buf[0] = 0x10u;
+	i2c0_tx_buf[1] = 0x21u;
+
+	rc = i2c_write_read(i2c0_dev, i2c_addr, (const void *)i2c0_tx_buf, nwrite,
+			    (void *)i2c0_rx_buf, nread);
+	LOG_INF("I2C0 write-read from I2C1 API returned (%d)", rc);
+
+	LOG_HEXDUMP_INF(i2c0_tx_buf, nwrite, "i2c0_tx_buf");
+	LOG_HEXDUMP_INF(i2c0_rx_buf, nread, "i2c0_rx_buf");
+
+	LOG_INF("I2C1 target RX index = 0x%0x", target_i2c_addr1_rx_idx);
+	LOG_INF("I2C1 target TX index = 0x%0x", target_i2c_addr1_tx_idx);
+
+	LOG_HEXDUMP_INF(target_i2c_addr1_rx_buf, 64u, "I2C1 target RX buffer");
+	LOG_HEXDUMP_INF(target_i2c_addr1_tx_buf, 64u, "I2C1 target TX buffer");
 #endif
 
 	LOG_INF("Program End");
@@ -252,7 +334,7 @@ static int i2c1_targ_read_req_cb(struct i2c_target_config *config, uint8_t *val)
 			target_i2c_addr1_tx_idx++;
 		}
 	} else if (targ_addr == I2C1_TARG_ADDR2) {
-		if (target_i2c_addr2_rx_idx < 256u) {
+		if (target_i2c_addr2_rx_idx < TARGET_I2C_ADDR2_RX_BUF_SIZE) {
 			targ_data = target_i2c_addr2_tx_buf[target_i2c_addr2_tx_idx];
 			target_i2c_addr2_tx_idx++;
 		}
@@ -287,7 +369,7 @@ static int i2c1_targ_read_proc_cb(struct i2c_target_config *config, uint8_t *val
 			rc = -E2BIG;
 		}
 	} else if (targ_addr == I2C1_TARG_ADDR2) {
-		if (target_i2c_addr2_tx_idx < 256u) {
+		if (target_i2c_addr2_tx_idx < TARGET_I2C_ADDR2_RX_BUF_SIZE) {
 			targ_data = target_i2c_addr2_tx_buf[target_i2c_addr2_tx_idx];
 			target_i2c_addr2_tx_idx++;
 		} else {
@@ -320,7 +402,7 @@ static int i2c1_targ_write_req_cb(struct i2c_target_config *config)
 			rc = -E2BIG;
 		}
 	} else if (targ_addr == I2C1_TARG_ADDR2) {
-		if (target_i2c_addr2_tx_idx >= 256u) {
+		if (target_i2c_addr2_tx_idx >= TARGET_I2C_ADDR2_TX_BUF_SIZE) {
 			rc = -E2BIG;
 		}
 	} else {
@@ -345,13 +427,19 @@ static int i2c1_targ_write_recv_cb(struct i2c_target_config *config, uint8_t val
 		if (target_i2c_addr1_rx_idx < 256u) {
 			target_i2c_addr1_rx_buf[target_i2c_addr1_rx_idx] = val;
 			target_i2c_addr1_rx_idx++;
+			if (target_i2c_addr1_rx_idx >= 256u) {
+				rc = -E2BIG;
+			}
 		} else {
 			rc = -E2BIG;
 		}
 	} else if (targ_addr == I2C1_TARG_ADDR2) {
-		if (target_i2c_addr2_rx_idx < 256u) {
+		if (target_i2c_addr2_rx_idx < TARGET_I2C_ADDR2_RX_BUF_SIZE) {
 			target_i2c_addr2_rx_buf[target_i2c_addr2_rx_idx] = val;
 			target_i2c_addr2_rx_idx++;
+			if (target_i2c_addr2_rx_idx >= TARGET_I2C_ADDR2_RX_BUF_SIZE) {
+				rc = -E2BIG;
+			}
 		} else {
 			rc = -E2BIG;
 		}
@@ -381,12 +469,72 @@ static int i2c1_targ_stop_cb(struct i2c_target_config *config)
 static void i2c1_targ_buf_write_recv_cb(struct i2c_target_config *config, uint8_t *ptr,
 					uint32_t len)
 {
-	return;
+	void *dest = NULL;
+	uint32_t copy_len = 0;
+	uint16_t targ_addr = 0;
+
+	if (config == NULL) {
+		LOG_ERR("I2C1 TBWR_CB: target config is NULL");
+		return;
+	}
+
+	LOG_INF("I2C1 TBWR_CB: addr=0x%0x", config->address);
+	LOG_INF("I2C1 TBWR_CB: ptr=0x%0x, len=%u", (uint32_t)ptr, len);
+#if 0
+	if ((ptr != NULL) && (len != 0)) {
+		LOG_HEXDUMP_INF(ptr, len, "recv_data");
+	}
+#endif
+	copy_len = len;
+	targ_addr = config->address;
+
+	if (targ_addr == I2C1_TARG_ADDR1) {
+		if (copy_len > sizeof(target_i2c_addr1_rx_buf)) {
+			copy_len = sizeof(target_i2c_addr1_rx_buf);
+		}
+
+		target_i2c_addr1_rx_idx = copy_len;
+		dest = (void *)target_i2c_addr1_rx_buf;
+
+	} else if (targ_addr == I2C1_TARG_ADDR2) {
+		if (copy_len > TARGET_I2C_ADDR2_RX_BUF_SIZE) {
+			copy_len = TARGET_I2C_ADDR2_RX_BUF_SIZE;
+		}
+
+		target_i2c_addr2_rx_idx = copy_len;
+		dest = (void *)target_i2c_addr2_rx_buf;
+	} else {
+		LOG_ERR("I2C1 TBWR_CB: target address incorrect");
+	}
+
+	if (dest != NULL) {
+		memcpy(dest, ptr, copy_len);
+	}
 }
 
 static int i2c1_targ_buf_read_req_cb(struct i2c_target_config *config, uint8_t **ptr, uint32_t *len)
 {
-	return -ENOTSUP;
+	int rc = 0;
+	uint16_t targ_addr = 0;
+
+	if (config == NULL) {
+		LOG_ERR("I2C1 TBRD_CB: target config is NULL");
+		return -EINVAL;
+	}
+
+	targ_addr = config->address;
+
+	if (targ_addr == I2C1_TARG_ADDR1) {
+		*ptr = &target_i2c_addr1_tx_buf[0];
+		*len = 64u;
+	} else if (targ_addr == I2C1_TARG_ADDR2) {
+		*ptr = &target_i2c_addr2_tx_buf[0];
+		*len = TARGET_I2C_ADDR2_TX_BUF_SIZE;
+	} else {
+		rc = -ENXIO;
+	}
+
+	return rc;
 }
 #endif /* CONFIG_I2C_TARGET_BUFFER_MODE */
 #endif /* CONFIG_I2C_TARGET */
