d8f2e64d87d36b6453637869e013a4c0fcf582a4
Scott Worley <scott.worley@microchip.com>
2025-09-18 11:07:56 -0400

    soc: microchip: mec: Share more common code and definitions
    We add more common peripheral register definitions.
    Convert more SoC include and initialization to use
    common code.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

--- soc/microchip/mec/mec174x/soc.h
+++ soc/microchip/mec/mec174x/soc.h
@@ -4,24 +4,26 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef __MEC5_SOC_H
-#define __MEC5_SOC_H
+#ifndef __SOC_MICROCHIP_MEC_MEC174X_SOC_H
+#define __SOC_MICROCHIP_MEC_MEC174X_SOC_H
 
 #define SYSCLK_DEFAULT_IOSC_HZ MHZ(96)
 
 #ifndef _ASMLANGUAGE
 
-#include "device_mec5.h"
+#include <device_mec5.h>
 
 /* common SoC API */
-#include "soc_dt.h"
-#include "soc_ecia.h"
-#include "soc_espi_channels.h"
-#include "soc_gpio.h"
-#include "soc_pcr.h"
-#include "soc_pins.h"
-#include "reg/xec_i2c_regs.h"
+#include <soc_dt.h>
+#include <soc_ecia.h>
+#include <soc_espi_channels.h>
+#include <soc_gpio.h>
+#include <soc_mmcr.h>
+#include <soc_pcr.h>
+#include <soc_pins.h>
+
+#define XEC_DMAC_MAX_CHAN 16u
+#define XEC_DMAC_CHAN_MSK 0xffffu
 
 #endif
-
 #endif
