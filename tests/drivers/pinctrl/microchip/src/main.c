5e2dfdebf6644a1c0a70c1ea5f698c6dcc80b51b
Scott Worley <scott.worley@microchip.com>
2025-12-11 13:33:15 -0500

    tests: drivers: pinctrl: microchip: Add MEC parts to test
    In-progress add Microchip MEC to PINCTRL test.
    Note: This test only checks the pinmux value created by
    device tree.  It does not check the pin configuration.
    We should add checking GPIO HW registers programming.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

--- tests/drivers/pinctrl/microchip/src/main.c
+++ tests/drivers/pinctrl/microchip/src/main.c
@@ -12,6 +12,59 @@
 PINCTRL_DT_DEV_CONFIG_DECLARE(TEST_DEVICE);
 static const struct pinctrl_dev_config *pcfg = PINCTRL_DT_DEV_CONFIG_GET(TEST_DEVICE);
 
+#ifdef CONFIG_TEST_PINCTRL_MCHP_MEC
+
+ZTEST(pinctrl_mchp_mec, test_slew_rate)
+{
+	const struct pinctrl_state *scfg;
+	uint32_t val = 0;
+
+	scfg = &pcfg->states[0];
+
+	val = (scfg->pins[1] >> MCHP_XEC_SLEW_RATE_POS) & MCHP_XEC_SLEW_RATE_MSK0;
+	zassert_equal(val, MCHP_XEC_SLEW_RATE_SLOW0);
+}
+
+ZTEST(pinctrl_mchp_mec, test_drive_strength)
+{
+	const struct pinctrl_state *scfg;
+	uint32_t val = 0;
+
+	scfg = &pcfg->states[0];
+
+	val = (scfg->pins[2] >> MCHP_XEC_DRV_STR_POS) & MCHP_XEC_DRV_STR_MSK0;
+	zassert_equal(val, MCHP_XEC_DRV_STR0_4X);
+}
+
+ZTEST(pinctrl_mchp_mec, test_pullup)
+{
+	const struct pinctrl_state *scfg;
+
+	scfg = &pcfg->states[0];
+
+	zassert_equal(((scfg->pins[3] >> MCHP_XEC_PU_POS) & 1u), 1);
+}
+
+ZTEST(pinctrl_mchp_mec, test_pulldown)
+{
+	const struct pinctrl_state *scfg;
+
+	scfg = &pcfg->states[0];
+
+	zassert_equal(((scfg->pins[4] >> MCHP_XEC_PD_POS) & 1u), 1);
+}
+
+ZTEST(pinctrl_mchp_mec, test_apply_state)
+{
+	int ret;
+
+	ret = pinctrl_apply_state(pcfg, PINCTRL_STATE_DEFAULT);
+	zassert_equal(ret, 0);
+}
+
+ZTEST_SUITE(pinctrl_mchp_mec, NULL, NULL, NULL, NULL, NULL);
+
+#else
 #define MCHP_PINCTRL_FLAG_GET(pincfg, pos) (((pincfg.pinflag) >> pos) & MCHP_PINCTRL_FLAG_MASK)
 
 ZTEST(pinctrl_mchp, test_pullup_pulldown_none)
@@ -74,3 +127,4 @@ ZTEST(pinctrl_mchp, test_apply_state)
 }
 
 ZTEST_SUITE(pinctrl_mchp, NULL, NULL, NULL, NULL, NULL);
+#endif /* CONFIG_TEST_PINCTRL_MCHP_MEC */
