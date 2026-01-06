1e6629d3b82834cbf1c387825768acd7469098cc
scott worley <scott.worley@microchip.com>
2026-01-05 22:58:16 -0500

    soc: microchip: mec: Disable MEC5 HAL for MEC1653B/174x/175x
    We disabled the Microchip MEC5 HAL for SoCs MEC1653B,
    MEC174x, and MEC175x. All driver use SoC register header
    files and Zephyr sys_io.h register access functions.
    
    Signed-off-by: scott worley <scott.worley@microchip.com>

--- soc/microchip/mec/mec165xb/soc.h
+++ soc/microchip/mec/mec165xb/soc.h
@@ -11,7 +11,36 @@
 
 #ifndef _ASMLANGUAGE
 
-#include <device_mec5.h>
+#define __FPU_PRESENT           0
+#define __MPU_PRESENT           CONFIG_CPU_HAS_ARM_MPU
+
+#define __CM4_REV               0x0201
+
+#define __VTOR_PRESENT          1
+#define __NVIC_PRIO_BITS        3
+#define __Vendor_SysTickConfig  0
+#define __FPU_DP                0
+#define __ICACHE_PRESENT        0
+#define __DCACHE_PRESENT        0
+#define __DTCM_PRESENT          0
+
+typedef enum {
+	/* ARM Cortex-M4 Specific Interrupt Numbers */
+	Reset_IRQn              = -15,
+	NonMaskableInt_IRQn     = -14,
+	HardFault_IRQn          = -13,
+	MemoryManagement_IRQn   = -12,
+	BusFault_IRQn           = -11,
+	UsageFault_IRQn         = -10,
+	SVCall_IRQn             = -5,
+	DebugMonitor_IRQn       = -4,
+	PendSV_IRQn             = -2,
+	SysTick_IRQn            = -1,
+	FIRST_PERIPH_IRQn       = 0,
+	MAX_IRQn = CONFIG_NUM_IRQS
+} IRQn_Type;
+
+#include <core_cm4.h>
 #include <soc_common.h>
 
 #endif
