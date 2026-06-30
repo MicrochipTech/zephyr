7acc87eeb50736b1583aa68f712d1f589ed82bb6
Scott Worley <scott.worley@microchip.com>
2026-06-30 08:19:01 -0400

    include: drivers: espi: Add Microchip XEC eSPI next-gen header
    We added an empty header for future Microchip XEC eSPI next-gen
    specific symbols and functions.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

---
+++ include/zephyr/drivers/espi/mchp_xec_espi_ng.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2026 Microchip Technology Inc
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_ESPI_MCHP_XEC_ESPI_NG_H_
+#define ZEPHYR_INCLUDE_DRIVERS_ESPI_MCHP_XEC_ESPI_NG_H_
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_ESPI_MCHP_XEC_ESPI_NG_H_ */
