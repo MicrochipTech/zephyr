b45059f45a1d9beab2db3e697d74e8ed385e5a53
i17752 <i17752@microchip.com>
2026-07-06 20:40:21 +0530

    samples: tfm_ipc: MEC176x bringup config
    Add MEC176x board overlay/config and a minimal PSA framework-version
    check to main.c for TF-M + Zephyr NS bring-up on mec176x_profpga.
    
    Bringup/dev change (CONFIG_INIT_ARCH_HW_AT_BOOT=n and partition trims);
    not intended as-is for upstream.

--- samples/tfm_integration/tfm_ipc/src/main.c
+++ samples/tfm_integration/tfm_ipc/src/main.c
@@ -1,86 +1,32 @@
 /*
- * Copyright (c) 2019,2020, 2021, 2023 Linaro Limited
- *
- * SPDX-License-Identifier: Apache-2.0
+ * Minimal TF-M IPC test for MEC176x bring-up
+ * Tests psa_framework_version() without crypto partition
  */
 
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
-#include "tfm_ns_interface.h"
-#ifdef TFM_PSA_API
-#include "psa_manifest/sid.h"
-#include "psa/crypto.h"
-#endif
-
-/**
- * \brief Retrieve the version of the PSA Framework API.
- *
- * \note This is a functional test only and doesn't
- *       mean to test all possible combinations of
- *       input parameters and return values.
- */
-static void tfm_get_version(void)
-{
-	uint32_t version;
-
-	version = psa_framework_version();
-	if (version == PSA_FRAMEWORK_VERSION) {
-		printk("The version of the PSA Framework API is %d.\n",
-		       version);
-	} else {
-		printk("The version of the PSA Framework API is not valid!\n");
-		return;
-	}
-}
-
-#ifdef TFM_PSA_API
-/**
- * \brief Retrieve the minor version of a RoT Service.
- */
-static void tfm_get_sid(void)
-{
-	uint32_t version;
-
-	version = psa_version(TFM_CRYPTO_SID);
-	if (version == PSA_VERSION_NONE) {
-		printk("RoT Service is not implemented or caller is not ");
-		printk("authorized to access it!\n");
-		return;
-	}
-
-	/* Valid version number */
-	printk("The PSA Crypto service minor version is %d.\n", version);
-}
-
-/**
- * \brief Generates random data using the TF-M crypto service.
- */
-void tfm_psa_crypto_rng(void)
-{
-	psa_status_t status;
-	uint8_t outbuf[256] = { 0 };
-
-	status = psa_generate_random(outbuf, 256);
-	printk("Generating 256 bytes of random data:");
-	for (uint16_t i = 0; i < 256; i++) {
-		if (!(i % 16)) {
-			printk("\n");
-		}
-		printk("%02X ", (uint8_t)(outbuf[i] & 0xFF));
-	}
-	printk("\n");
-}
-#endif
+#include "psa/client.h"
 
 int main(void)
 {
-	printk("TF-M IPC on %s\n", CONFIG_BOARD);
+	uint32_t version;
 
-	tfm_get_version();
-#ifdef TFM_PSA_API
-	tfm_get_sid();
-	tfm_psa_crypto_rng();
-#endif
+	printk("*** Booting TF-M + Zephyr NS on MEC176x ***\n");
+
+	version = psa_framework_version();
+	if (version == PSA_FRAMEWORK_VERSION) {
+		printk("SUCCESS: PSA Framework version = 0x%04x\n", version);
+	} else {
+		printk("FAIL: PSA Framework version mismatch (got 0x%x, expected 0x%x)\n",
+		       version, PSA_FRAMEWORK_VERSION);
+	}
+
+	printk("*** Test complete ***\n");
+
+	while (1) {
+		k_sleep(K_SECONDS(1));
+	}
+
 	return 0;
 }
