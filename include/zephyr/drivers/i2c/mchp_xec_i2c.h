b322bde4eaaf5b8f0bd02c5e4bffff3cd34df09a
Scott Worley <scott.worley@microchip.com>
2026-08-11 12:34:45 -0400

    drivers: i2c: microchip: XEC I2Cv3 byte mode driver fixes
    We fixed Microchip I2C v3.8 byte mode driver issues found in
    testing. We implemented state capture to facilite debugging
    issues reported by users. State capture allows the user to
    recompile with state capture on, reproduce the error, and
    supply the state capture to MCHP to facilitate debug.
    We implemented the side-band port get/set APIs similar to
    the I2Cv3-NL driver. We expect these APIs to be only used
    by customers who have boards where I2C ports are controlled
    by jumpers and must be discovered at runtime.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

--- include/zephyr/drivers/i2c/mchp_xec_i2c.h
+++ include/zephyr/drivers/i2c/mchp_xec_i2c.h
@@ -10,14 +10,24 @@
 
 #include <zephyr/device.h>
 
-int mchp_xec_i2c_nl_port_get(const struct device *i2c_dev, uint8_t *port);
-
-int mchp_xec_i2c_nl_port_set(const struct device *i2c_dev, uint8_t port);
+#ifdef CONFIG_I2C_MCHP_XEC_V3_NL
+int mchp_xec_i2c_nl_port_get(const struct device *i2c_port_dev, uint8_t *port);
+int mchp_xec_i2c_nl_port_set(const struct device *i2c_port_dev, uint8_t port);
 
 #ifdef CONFIG_I2C_MCHP_XEC_V3_NL_STATE_CAPTURE
-int mchp_xec_i2c_nl_clear_capture(const struct device *i2c_nl_ctrl_dev);
-int mchp_xec_i2c_nl_copy_capture(const struct device *i2c_nl_ctrl_dev, uint8_t *capdest,
-				 size_t capdest_size);
+int mchp_xec_i2c_nl_clear_capture(const struct device *port);
+int mchp_xec_i2c_nl_copy_capture(const struct device *port, uint8_t *capdest, size_t capdest_size);
+#endif
+#endif
+
+#ifdef CONFIG_I2C_MCHP_XEC_V3_BM
+int mchp_xec_i2c_bm_port_get(const struct device *i2c_port_dev, uint8_t *port);
+int mchp_xec_i2c_bm_port_set(const struct device *i2c_port_dev, uint8_t port);
+
+#ifdef CONFIG_I2C_MCHP_XEC_V3_BM_STATE_CAPTURE
+int mchp_xec_i2c_bm_clear_capture(const struct device *port);
+int mchp_xec_i2c_bm_copy_capture(const struct device *port, uint8_t *capdest, size_t capdest_size);
+#endif
 #endif
 
 #endif /* ZEPHYR_INCLUDE_DRIVERS_I2C_MCHP_XEC_I2C_NL_H_ */
