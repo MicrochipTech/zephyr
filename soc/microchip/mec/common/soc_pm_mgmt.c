57b0990611f6a983b81b095a17692445f564e0bd
Scott Worley <scott.worley@microchip.com>
2026-08-11 12:24:23 -0400

    soc: microchip: mec: Common PM deep sleep update
    We updated the common PM management code for Microchip
    MEC174x/175x/165xB SoCs. This is an interim change as
    the PM implementation is re-evaluated. The important
    change in this commit is the sequence adjust interrupt
    masking. ARM Cortex-M4 NVIC will not wake if an interrupt
    is masked using the BASEPRI register. Zephyr's PM subsystem
    uses BASEPRI to mask interrupts before calling the SoC PM
    state entry function. ARM's recommendations are to mask
    all maskable interrupts using PRIMASK, set BASEPRI to 0
    allowing any pending interrupt to wake the core. This
    work-around requries SoC to implement PM post ops exit
    to clear PRIMASK. Between wake and PM post ops interrupts
    are masked by PRIMASK allowing the PM subystem to call
    any registered callbacks (application PM notification or
    PM_DEVICE capable drivers). Note: there will be more
    changes as we are evaluation moving CONFIG_PM only to
    require the application to register a PM notifier callback
    and perform any special peripheral handling. The applcation
    layer knows which peripheral's it has enabled and the
    drivers they use.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

--- soc/microchip/mec/common/soc_pm_mgmt.c
+++ soc/microchip/mec/common/soc_pm_mgmt.c
@@ -10,6 +10,13 @@
 #include <zephyr/pm/pm.h>
 #include <zephyr/sys/sys_io.h>
 
+#include "soc_pm_periph.h"
+
+#define XEC_PM_DEBUG_GPIO_MARKER
+#define XEC_PM_DEBUG_CLK_REQ_VBAT
+
+#define XEC_PM_DEBUG_CLK_REQ_VBAT_LOC 0
+
 #define XEC_PCR_REG_BASE DT_REG_ADDR(DT_NODELABEL(pcr))
 
 #define XEC_PCR_SLP_CR_OFS          0
@@ -25,39 +32,67 @@
 #define XEC_BASIC_TIMER_CR_OFS    0x10U
 #define XEC_BASIC_TIMER_CR_EN_POS 0
 
+#define XEC_I2C_INSTANCES 5
+#define XEC_I2C0_REG_BASE DT_REG_ADDR(DT_NODELABEL(i2c_smb_0))
+#define XEC_I2C_INST_OFS  0x400U
+
+#define XEC_I2C_CONFIG_OFS   0x28U
+#define XEC_I2C_WAKE_STS_OFS 0x60U
+#define XEC_I2C_WAKE_EN_OFS  0x64U
+
 #if DT_NODE_EXISTS(DT_NODELABEL(uart3))
-#define MEC165XB_UART_INSTANCES 4
+#define XEC_UART_INSTANCES 4
 #elif DT_NODE_EXISTS(DT_NODELABEL(uart2))
-#define MEC165XB_UART_INSTANCES 3
+#define XEC_UART_INSTANCES 3
 #else
-#define MEC165XB_UART_INSTANCES 2
+#define XEC_UART_INSTANCES 2
 #endif
 
 #define XEC_UART0_REG_BASE DT_REG_ADDR(DT_NODELABEL(uart0))
 
-#define XEC_PM_DEEP_SLP_DEBUG
-#define XEC_PM_DEEP_SLP_VB_DEBUG
+/* PM debug */
+#ifdef XEC_PM_DEBUG_GPIO_MARKER
+#define MEC_GPIO_BASE DT_REG_ADDR(DT_NODELABEL(pinctrl))
 
-#define XEC_VBMEM_BASE 0x4000A800U
+#define XEC_PM_ENTRY_EXIT_GPIO 0241
 
-#define XEC_PM_DEEP_SLP_GPIO 0241 /* octal */
+#define XEC_PM_EE_GPIO_CR_ADDR MEC_GPIO_CR1_ADDR(MEC_GPIO_BASE, XEC_PM_ENTRY_EXIT_GPIO)
 
-#define XEC_PM_DEEP_SLP_GPIO_CR_ADDR MEC_GPIO_CR1_ADDR(0x40081000U, XEC_PM_DEEP_SLP_GPIO)
+#define XEC_PM_EE_GPIO_DRV_LO 0x240U
+#define XEC_PM_EE_GPIO_DRV_HI 0x10240U
 
-#ifdef XEC_PM_DEEP_SLP_DEBUG
-#define XEC_DBG_PM_DEEP_SLP_ASSERT   sys_write32(0x00240, XEC_PM_DEEP_SLP_GPIO_CR_ADDR)
-#define XEC_DBG_PM_DEEP_SLP_DEASSERT sys_write32(0x10240, XEC_PM_DEEP_SLP_GPIO_CR_ADDR)
+#define XEC_PM_DBG_DS_ENTER() sys_write32(XEC_PM_EE_GPIO_DRV_LO, XEC_PM_EE_GPIO_CR_ADDR)
+#define XEC_PM_DBG_DS_EXIT() sys_write32(XEC_PM_EE_GPIO_DRV_HI, XEC_PM_EE_GPIO_CR_ADDR)
+
 #else
-#define XEC_DBG_PM_DEEP_SLP_ASSERT
-#define XEC_DBG_PM_DEEP_SLP_DEASSERT
+#define XEC_PM_DBG_DS_ENTER()
+#define XEC_PM_DBG_DS_EXIT()
+#endif
+
+#ifdef XEC_PM_DEBUG_CLK_REQ_VBAT
+static void soc_pm_dbg_clk_req_save_to_vbat(void)
+{
+	uintptr_t pcr_base = (uintptr_t)XEC_PCR_REG_BASE + 0x50U;
+	uintptr_t vbmem_base = ((uintptr_t)DT_REG_ADDR(DT_NODELABEL(bbram)) +
+				XEC_PM_DEBUG_CLK_REQ_VBAT_LOC);
+
+	for (uint32_t i = 0; i < 20U; i += 4U) {
+		uint32_t clk_req = sys_read32(pcr_base + i);
+
+		sys_write32(clk_req, vbmem_base + i);
+	}
+}
+#define XEC_PM_DEBUG_CLK_REQ_VBAT_SAVE() soc_pm_dbg_clk_req_save_to_vbat()
+#else
+#define XEC_PM_DEBUG_CLK_REQ_VBAT_SAVE()
 #endif
 
 static uint8_t basic_timer_cr_save[XEC_BASIC_TIMER_INSTANCES];
-static uint8_t uart_actv_save[MEC165XB_UART_INSTANCES];
+static uint8_t uart_actv_save[XEC_UART_INSTANCES];
 
 static void save_basic_timers(void)
 {
-	mm_reg_t rb = (mm_reg_t)(XEC_BASIC_TIMER0_REG_BASE);
+	uintptr_t rb = (uintptr_t)(XEC_BASIC_TIMER0_REG_BASE);
 
 	for (uint32_t n = 0; n < XEC_BASIC_TIMER_INSTANCES; n++) {
 		uint32_t temp = sys_read32(rb + XEC_BASIC_TIMER_CR_OFS);
@@ -70,7 +105,7 @@ static void save_basic_timers(void)
 
 static void restore_basic_timers(void)
 {
-	mm_reg_t rb = (mm_reg_t)(XEC_BASIC_TIMER0_REG_BASE);
+	uintptr_t rb = (uintptr_t)(XEC_BASIC_TIMER0_REG_BASE);
 
 	for (uint32_t n = 0; n < XEC_BASIC_TIMER_INSTANCES; n++) {
 		if (basic_timer_cr_save[n] != 0) {
@@ -82,9 +117,9 @@ static void restore_basic_timers(void)
 
 static void save_uarts(void)
 {
-	mm_reg_t rb = (mm_reg_t)(XEC_UART0_REG_BASE);
+	uintptr_t rb = (uintptr_t)(XEC_UART0_REG_BASE);
 
-	for (uint32_t n = 0; n < MEC165XB_UART_INSTANCES; n++) {
+	for (uint32_t n = 0; n < XEC_UART_INSTANCES; n++) {
 		uart_actv_save[n] = sys_read8(rb + XEC_UART_LD_ACT_OFS);
 		sys_write8(0, rb + XEC_UART_LD_ACT_OFS);
 		rb += XEC_UART_SPACING;
@@ -93,12 +128,25 @@ static void save_uarts(void)
 
 static void restore_uarts(void)
 {
-	mm_reg_t rb = (mm_reg_t)(XEC_UART0_REG_BASE);
+	uintptr_t rb = (uintptr_t)(XEC_UART0_REG_BASE);
 
-	for (uint32_t n = 0; n < MEC165XB_UART_INSTANCES; n++) {
+	for (uint32_t n = 0; n < XEC_UART_INSTANCES; n++) {
 		sys_write8(uart_actv_save[n], rb + XEC_UART_LD_ACT_OFS);
 		rb += XEC_UART_SPACING;
 	}
+}
+
+/* Enable peripherals capable of waking from deep sleep when a packet is received from
+ * and external source: I2C, I3C, eSPI, and SPI target.
+ * GIRQ22 is used to turn the PLL on long enough to decide if the packet is meant for this
+ * SoC. If it is, the peripheral will asserts its specific GIRQx interrupt which wakes the
+ * SoC CPU. If the packet is not for this device, HW will turn the PLL back off not waking
+ * the SoC CPU.
+ */
+static void prepare_wake_devices(void)
+{
+	soc_ecia_girq_status_clear_bm(MCHP_MEC_ECIA_GIRQ22, UINT32_MAX);
+	soc_ecia_girq_ctrl_bm(MCHP_MEC_ECIA_GIRQ22, UINT32_MAX, MCHP_MEC_ECIA_GIRQ_EN);
 }
 
 /* Peripheral's that don't obey PCR sleep signals
@@ -117,7 +165,7 @@ static void soc_deep_sleep_periph_restore(void)
 }
 
 /*
- * Enable deep sleep mode in CM4 and MEC172x.
+ * Enable deep sleep mode in CM4 and XEC PCR.
  * Enable CM4 deep sleep and sleep signals assertion on WFI.
  * Set MCHP Heavy sleep (PLL OFF when all CLK_REQ clear) and SLEEP_ALL
  * to assert SLP_EN to all peripherals on WFI.
@@ -127,47 +175,66 @@ static void soc_deep_sleep_periph_restore(void)
  * On wake
  * Clear SLEEP_ALL manually since we are not vectoring to an ISR until
  * PM post ops. This de-asserts peripheral SLP_EN signals.
+ * 
+ * NOTE: Cortex-M4 NVIC pending interrupts will not wake the core if they are masked.
+ * Zephyr PM subsystem masks interrupts using BASEPRI.
+ * Default: CM4 priority 0(highest) is unmasked all lower priorities are masked using BASEPRI.
+ * If CONFIG_ZERO_LATENCY_IRQS is enabled PM subsystem will leave priorities 0(highest) through
+ * 1 + CONFIG_ZERO_LATENCY_IRQS unmasked.
+ * The issue is Cortex-M4 NVIC will not wake the core if a pending interrupt's priority is masked
+ * in BASEPRI.
+ * Work-around recommended by ARM and use by other Zephyr Cortex-M3/M4 SoC.
+ * Mask all maskable interrupts using PRIMASK bit.
+ * Set BASEPRI to 0 to unmask all priority levels.
+ * Any pending interrupt in the NVIC will wake from WFI. If the interrupt is pending when WFI
+ * executes, WFI is treated as a NOP.
+ * After WFI we don't enter any ISR because PRIMASK is 1. The XEC SoC is on ring oscillator until
+ * its PLL locks. The ring oscillator clock is not stable for some peripherals (UART, etc.). We
+ * spin until lock (maximum 3 ms for MEC172x,4x,5x series).
+ * We restore HW and allow Zephyr PM subsystem to restore any HW (PM_DEVICE).
+ * On PM exit post ops we clear PRIMASK to allow servicing of pendind interrupts. 
  */
 static void z_power_soc_deep_sleep(void)
 {
-	mm_reg_t pcrbase = (mm_reg_t)(XEC_PCR_REG_BASE);
+	uint32_t pcrbase = (uint32_t)(XEC_PCR_REG_BASE);
 	uint32_t msk = BIT(XEC_PCR_SLP_CR_DEEP_SLP_POS) | BIT(XEC_PCR_SLP_CR_ALL_POS);
 	uint32_t val = BIT(XEC_PCR_SLP_CR_DEEP_SLP_POS) | BIT(XEC_PCR_SLP_CR_ALL_POS);
 
-	__disable_irq();
+	/* Cortex-Mx wake sequence recommended by ARM */
+	__disable_irq(); /* Prevent immediate ISR entry via PRIMASK */
+	irq_unlock(0); /* Set BASEPRI to 0 */
+	__DSB();
+	__ISB();
+
+	XEC_PM_DBG_DS_ENTER();
 
 	XEC_DBG_PM_DEEP_SLP_ASSERT;
 
 	soc_deep_sleep_periph_save();
+	prepare_wake_devices();
 
+	/* Enable Cortex-M4 to assert SLEEP_DEEP signal on WFI */
 	SCB->SCR |= BIT(SCB_SCR_SLEEPDEEP_Pos);
 
+	/* Enable MEC PCR to assert all peripheral SLP_EN signals on WFI */
 	soc_mmcr_mask_set(pcrbase + XEC_PCR_SLP_CR_OFS, val, msk);
 
-#ifdef XEC_PM_DEEP_SLP_VB_DEBUG
-	uint32_t r = sys_read32(XEC_PCR_CLK_REQ_BASE);
+	XEC_PM_DEBUG_CLK_REQ_VBAT_SAVE(); /* record PCR peripheral CLK_REQ signals */
 
-	sys_write32(r, XEC_VBMEM_BASE);
-	r = sys_read32(XEC_PCR_CLK_REQ_BASE + 4U);
-	sys_write32(r, XEC_VBMEM_BASE + 4U);
-	r = sys_read32(XEC_PCR_CLK_REQ_BASE + 8U);
-	sys_write32(r, XEC_VBMEM_BASE + 8U);
-	r = sys_read32(XEC_PCR_CLK_REQ_BASE + 0xCU);
-	sys_write32(r, XEC_VBMEM_BASE + 0xCU);
-	r = sys_read32(XEC_PCR_CLK_REQ_BASE + 0x10U);
-	sys_write32(r, XEC_VBMEM_BASE + 0x10U);
-#endif
-	__set_BASEPRI(0);
 	__DSB();
+	__ISB();
 	__WFI(); /* triggers sleep hardware */
 	__NOP();
 	__NOP();
 
+	/* wake */
 	soc_mmcr_mask_set(pcrbase + XEC_PCR_SLP_CR_OFS, 0, msk);
 
 	SCB->SCR &= ~BIT(SCB_SCR_SLEEPDEEP_Pos);
 
-	/* Wait for PLL to lock */
+	XEC_PM_DBG_DS_EXIT();
+
+	/* Wait for PLL to lock. Maximum is 3ms for current MEC parts */
 	while (sys_test_bit(pcrbase + XEC_PCR_OSC_ID_OFS, XEC_PCR_OSC_ID_PLL_LOCK_POS) == 0) {
 		__NOP();
 	}
@@ -176,6 +243,7 @@ static void z_power_soc_deep_sleep(void)
 
 	XEC_DBG_PM_DEEP_SLP_DEASSERT;
 }
+#endif
 
 /* NOTE: Zephyr kernel does not block all interrupts.
  * We use compiler instrisic to disable all interrupts except unmaskable
@@ -247,7 +315,7 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
  */
 void pm_state_exit_post_ops(enum pm_state state, uint8_t substate_id)
 {
-	__enable_irq();
+	__DSB();
 	__ISB();
-	irq_unlock(0);
+	__enable_irq(); /* Clear PRIMASK */
 }
