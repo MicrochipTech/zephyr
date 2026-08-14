423cc710bdbef388286db0e2c7ce93580e4debbb
Scott Worley <scott.worley@microchip.com>
2026-08-14 12:33:29 -0400

    samples: boards: microchip: xec_i2c_v3: ctrl_port_switch: Add PM
    We enabled PM for this sample, added code to print the suspend
    latency, and removed unused board from test.yaml
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

--- samples/boards/microchip/xec_i2c_v3/ctrl_port_switch/src/main.c
+++ samples/boards/microchip/xec_i2c_v3/ctrl_port_switch/src/main.c
@@ -17,6 +17,9 @@
 #include <zephyr/random/random.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/logging/log_ctrl.h>
+#include <zephyr/pm/pm.h>
+#include <zephyr/pm/state.h>
+
 LOG_MODULE_REGISTER(app, CONFIG_LOG_DEFAULT_LEVEL);
 
 /* #define APP_TEST_LTC2489 */
@@ -148,6 +151,15 @@ int main(void)
 	LOG_INF("I2C Ctrl0 compatible: %s", DT_PROP_BY_IDX(I2C_CTRL0_NODE, compatible, 0));
 #else
 	LOG_INF("I2C Ctrl0 does not have a compatible!");
+#endif
+#if defined(CONFIG_PM) || defined(CONFIG_PM_DEVICE)
+	/* param1 = CPU_ID = 0, param2 = PM state, param3 = PM substate = 0 */
+	const struct pm_state_info *susp_pm_info = pm_state_get(0, PM_STATE_SUSPEND_TO_IDLE, 0);
+	if (susp_pm_info != NULL) {
+		LOG_INF("PM Suspend min-residency-us = %u", susp_pm_info->min_residency_us);
+	} else {
+		LOG_ERR("Error getting suspend PM state info");
+	}
 #endif
 	log_flush();
 
